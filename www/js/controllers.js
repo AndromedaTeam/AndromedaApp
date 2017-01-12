cbc7640290b8e936f9ff795da962f43f62578879
Sree <cognizantcontestapp@gmail.com>
2017-01-12 23:58:53 +0530

    added email based authentication

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -36,15 +36,50 @@ angular.module('app.controllers', [])
     }
 ])
 
-.controller('loginCtrl', ['$scope', '$stateParams', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('loginCtrl', ['$scope', '$rootScope', '$stateParams', '$state', '$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function($scope, $stateParams, $state) {
+    function($scope, $rootScope, $stateParams, $state, $ionicPopup) {
+
+        // $scope.loginWithUserCredentials = function() {
+        //     console.log("here");
+        //     $state.transitionTo("tabsController.polling");
+        // }
+
+
+        $scope.user = {
+            email: this.email,
+            password: this.password
+        };
 
         $scope.loginWithUserCredentials = function() {
-            console.log("here");
-            $state.transitionTo("tabsController.polling");
-        }
+            var email = this.user.email;
+            var password = this.user.password;
+
+            /* Check user fields*/
+            if (!email || !password) {
+                var errorMessage = "Email or Password is incorrect!!";
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Oops!',
+                    template: errorMessage
+                });
+                return;
+            }
+            firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(function(result) {
+                    $state.transitionTo("tabsController.polling");
+                })
+                .catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    var alertPopup = $ionicPopup.alert({
+                        title: 'Oops!',
+                        template: errorMessage
+                    });
+                });
+
+        };
 
         $scope.loginGoogle = function() {
             var provider = new firebase.auth.GoogleAuthProvider();
@@ -54,7 +89,6 @@ angular.module('app.controllers', [])
             });
             firebase.auth().signInWithPopup(provider).then(function(result) {
                 var token = result.credential.accessToken;
-                var user = result.user;
                 var user = result.user;
                 var providerData = user.providerData[0];
                 firebase.database().ref('users/' + providerData.displayName).set({ Email: providerData.email, PhotoURL: providerData.photoURL, uid: providerData.uid });
@@ -69,24 +103,40 @@ angular.module('app.controllers', [])
                 var email = error.email;
                 // The firebase.auth.AuthCredential type that was used.
                 var credential = error.credential;
-                // ...
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Oops!',
+                    template: errorMessage
+                });
             });
         }
 
     }
 ])
 
-.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('signupCtrl', ['$scope', '$stateParams', '$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function($scope, $stateParams) {
+    function($scope, $stateParams, $ionicPopup) {
         $scope.user = {
-            name : "",
-            email: "",
-            password: ""
+            name: this.name,
+            email: this.email,
+            password: this.password
         }
-        $scope.signupWithEmail = function(){
-            console.log($scope.user);
+        $scope.signupWithEmail = function() {
+            firebase.auth().createUserWithEmailAndPassword($scope.user.email, $scope.user.password).then(function(result) {
+                var providerData = result.providerData[0];
+                firebase.database().ref('users/' + $scope.user.name).set({ Email: providerData.email, PhotoURL: providerData.photoURL, uid: providerData.uid });
+                console.log(providerData);
+            }).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Oops!',
+                    template: errorMessage
+                });
+            });
         }
     }
 ])
