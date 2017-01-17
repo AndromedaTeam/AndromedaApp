bf2f5f375af672c62cea49374635685568ca82dd
Sree <cognizantcontestapp@gmail.com>
2017-01-18 00:51:51 +0530

    ui enhancements

--- www/controllers/administrationCtrl.js
+++ www/controllers/administrationCtrl.js
@@ -1,12 +1,13 @@
-digitalVotingApp.controller('administrationCtrl', ['$scope', '$stateParams', '$firebaseArray', '$state', '$rootScope','$ionicPopup','$firebaseObject',
-    function($scope, $stateParams, $firebaseArray, $state, $rootScope,$ionicPopup,$firebaseObject) {
+digitalVotingApp.controller('administrationCtrl', ['$scope', '$stateParams', '$firebaseArray', '$state', '$rootScope', '$ionicPopup', '$firebaseObject',
+    function($scope, $stateParams, $firebaseArray, $state, $rootScope, $ionicPopup, $firebaseObject) {
         var ref = firebase.database().ref('users/');
         var list = $firebaseArray(ref);
         $scope.users = list;
 
         $scope.event = {
             name: this.name,
-            desc: this.desc
+            desc: this.desc,
+            id: null
         };
 
         // $scope.user = firebase.auth().currentUser;
@@ -27,67 +28,75 @@ digitalVotingApp.controller('administrationCtrl', ['$scope', '$stateParams', '$f
         $scope.manageEvents = function() {
             $state.transitionTo("manageEvents");
         };
-         $scope.editEvent = function(name,desc) {
-             $scope.event1 = {
-            name: name,
-            desc: desc
-        };
+        $scope.editEvent = function(name, desc) {
+            $scope.event1 = {
+                name: name,
+                desc: desc
+            };
             $state.transitionTo("editEvent");
         };
         $scope.submitEvent = function() {
-            var ref = firebase.database().ref('events/'+firebase.auth().currentUser.uid);
+            var ref = firebase.database().ref('events/' + firebase.auth().currentUser.uid);
             var list = $firebaseArray(ref);
-               list.$add({
-                    Name: $scope.event.name,
-                    Description: $scope.event.desc,
-                }).then(function() {
-                    var alertPopup = $ionicPopup.alert({
-                        title: 'Success!',
-                        template: "Event created successfully"
-                    });
-                     alertPopup.then(function(res) {
-                $state.transitionTo("manageEvents");
+            list.$add({
+                Name: $scope.event.name,
+                Description: $scope.event.desc,
+            }).then(function() {
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Success!',
+                    template: "Event created successfully"
+                });
+                alertPopup.then(function(res) {
+                    $state.transitionTo("manageEvents");
+                });
             });
-        }); 
         }
-        $scope.events=[];
-        $scope.poll={};
-           $scope.loadEvents=function(){
-               $scope.events=[];
-            var ref = firebase.database().ref('events/'+'R71XUPUN8JYu7yOKAdIIADBgmEh2');
-            var obj = $firebaseArray(ref);
-            obj.$loaded().then(function() {
-              angular.forEach(obj, function(value, key) {
-                 // $scope.poll.key=value;
-       $scope.events.push(value);
-       });
-            
+        $scope.events = [];
+        $scope.poll = {};
+        $scope.loadEvents = function() {
+            setTimeout(function() {
+                $scope.events = [];
+                var ref = firebase.database().ref('events/' + firebase.auth().currentUser.uid);
+                var obj = $firebaseArray(ref);
+                obj.$loaded().then(function() {
+                    angular.forEach(obj, function(value, key) {
+                        // $scope.poll.key=value;
+                        $scope.events.push(value);
+                    });
+                    console.log($scope.events);
+                });
+            }, 500);
+
+
+        };
+        $scope.selectedAll = false;
+        $scope.selected = {};
+        $scope.selectAll = function() {
+            $scope.selectedAll = !$scope.selectedAll;
+            for (var i = 0; i < $scope.events.length; i++) {
+                var item = $scope.events[i];
+
+                $scope.selected[item.Name] = $scope.selectedAll;
+            }
+        };
+        $scope.deleteEvent = function() {
+            var ref = firebase.database().ref('events/' + 'R71XUPUN8JYu7yOKAdIIADBgmEh2');
+            var list = $firebaseArray(ref);
+            list.$loaded().then(function() {
+                angular.forEach(list, function(item) {
+                    if ($scope.selected[item.Name] == true) {
+                        list.$remove(item).then(function(ref) {
+                            console.log(deleted); // true
+
+                        });
+                    }
+                });
+                $scope.loadEvents();
             });
         };
-        $scope.selectedAll=false;
-      $scope.selected = {};
-$scope.selectAll = function(){
-    $scope.selectedAll=!$scope.selectedAll;
-  for (var i = 0; i < $scope.events.length; i++) {
-    var item = $scope.events[i];
 
-    $scope.selected[item.Name] = $scope.selectedAll;
-  }
-};
-$scope.deleteEvent=function(){
-    var ref = firebase.database().ref('events/'+'R71XUPUN8JYu7yOKAdIIADBgmEh2');
-    var list = $firebaseArray(ref);
-    list.$loaded().then(function(){
-    angular.forEach(list,function(item){
-        if($scope.selected[item.Name]==true){
-    list.$remove(item).then(function(ref) {
- console.log(deleted);// true
-    
-});
+        $scope.openEvent = function() {
+            alert($scope.event.id)
         }
-    });
-$scope.loadEvents();
-    });
-};
     }
 ]);
--- www/controllers/profileCtrl.js
+++ www/controllers/profileCtrl.js
@@ -1,7 +1,7 @@
-digitalVotingApp.controller('profileCtrl', ['$scope', '$stateParams', '$state', '$ionicHistory', '$firebaseObject', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+digitalVotingApp.controller('profileCtrl', ['$scope', '$rootScope', '$stateParams', '$state', '$ionicHistory', '$firebaseObject', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function($scope, $stateParams, $state, $ionicHistory, $firebaseObject) {
+    function($scope, $rootScope, $stateParams, $state, $ionicHistory, $firebaseObject) {
 
         var ref = firebase.database().ref("users/" + firebase.auth().currentUser.uid);
         var user = $firebaseObject(ref);
@@ -13,14 +13,13 @@ digitalVotingApp.controller('profileCtrl', ['$scope', '$stateParams', '$state', 
         });
 
 
+
         //$('#profile_image').empty();
         //$('#profile_image').append('<img id="theImg" src="theImg.png" />')
         $scope.doLogout = function() {
+            $state.transitionTo("login");
             firebase.auth().signOut().then(function() {
-                // $ionicHistory.nextViewOptions({
-                //     disableBack: true
-                // });
-                $state.transitionTo("login");
+                $rootScope.$broadcast('logout');
             }, function(error) {
 
             });
